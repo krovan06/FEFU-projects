52b1b9b750b41d99c21f7c45d4af4bd07e9a4356
krovan <149927522+krovan06@users.noreply.github.com>
2024-11-05 17:06:50 +1000

    new react-quiz

--- SITE_TEST/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const button = document.getElementsByClassName('dropdown')[0];
-  const dropdownContent = document.getElementsByClassName('dropdown-content')[0];
-
-  button.addEventListener('click', function() {
-    dropdownContent.classList.toggle('show');
-  });
-});
